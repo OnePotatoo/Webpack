6bf5a96c93ec0150fce73ac202d5ec2d30db0f3c
yyh <958182321@qq.com>
2022-06-17 20:34:39 +0800

    完成了css和less

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { marquee } from "./marquee.js";
+marquee()
+
+import "./style/main.css"
+import "./style/main.less"
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { join } = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: "./src/main.js",  // 入口
+    output: {
+        path: join(__dirname, "lib"),  // 出口路径
+        filename: "index.js",  // 出口文件名
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 自定义打包的html模版,用绝对路径
+            template: join(__dirname, 'public/main.html')
+        })
+    ],
+    devServer: {
+        open: true,
+        port: 30000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/i,
+                use: ["style-loader", "css-loader", "less-loader"]
+            }
+        ],
+    },
+}
